fc7d1475e24036f234270b1140b62976f27277b0
Christian Knudsen <christian.knudsen@gmail.com>
2022-02-01 15:28:46 +0100

    Improved documentation for folketingsvalg

--- R/data.R
+++ R/data.R
@@ -1,5 +1,6 @@
 #' Folketingsvalg siden 1953
+#' @description Dates for Danish parlamentary elections since 1953-09-22
 #'
-#'
-#' @format A date vector containing dates for danish parlamentary elections since 1953
+#' @format A date vector with 24 dates
+#' @source https://www.ft.dk/-/media/sites/ft/pdf/folkestyret/valg-og-afstemninger/folketingsvalg-fra-1953.ashx
 "folketingsvalg"
