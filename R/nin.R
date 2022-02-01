5fe75b5414d96676a71d7d13b2bfac70243e8f2b
Christian Knudsen <christian.knudsen@gmail.com>
2022-02-01 12:50:49 +0100

    Exportable nin

--- R/nin.R
+++ R/nin.R
@@ -3,15 +3,7 @@
 #' @details details here
 #' @param param 1
 #' @param param 2
-#' @examples
-#' library(ggplot2)
-#'
-#' ggplot(airquality, aes(x = Day, y = Temp,
-#'      group = as.factor(Month), color = as.factor(Month))) +
-#'      geom_point(size = 2.5) +
-#'      scale_color_ucph() +
-#'      theme_kubdatalab()
-#' @rdname nin
+#' @rdname `%nin%`
 #' @export
 
 `%nin%` <- function (x, table) {
