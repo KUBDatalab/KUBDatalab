8f956b315f5fda44c72e58b6d4b2b954dad3771b
Christian Knudsen <christian.knudsen@gmail.com>
2022-02-01 12:46:15 +0100

    Documentation for nin added

--- R/nin.R
+++ R/nin.R
@@ -1,3 +1,19 @@
+#' @title Negated %in%
+#' @description Negated %in%
+#' @details details here
+#' @param param 1
+#' @param param 2
+#' @examples
+#' library(ggplot2)
+#'
+#' ggplot(airquality, aes(x = Day, y = Temp,
+#'      group = as.factor(Month), color = as.factor(Month))) +
+#'      geom_point(size = 2.5) +
+#'      scale_color_ucph() +
+#'      theme_kubdatalab()
+#' @rdname nin
+#' @export
+
 `%nin%` <- function (x, table) {
   match(x, table, nomatch = 0L) == 0L
 }
