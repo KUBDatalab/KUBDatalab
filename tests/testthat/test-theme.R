489b8abd1325fb1fa441b9f0699b0fbdc1c04f66
Christian Knudsen <christian.knudsen@gmail.com>
2022-02-02 10:25:41 +0100

    ucph palette unit tests added

--- tests/testthat/test-theme.R
+++ tests/testthat/test-theme.R
@@ -76,17 +76,44 @@ test_that("scale_fill_ucph works", {
 })
 
 ## Colors are correct ----
-test_that("ucph palette outputs correct colors", {
-  expect_equal(ucph_pal()(10),
-               c(  "#fed90f", ## #FFD235 simpsons yellow    #ffdb58 mustard    #fed90f metallic yellow
-                   "#424f46",  ## #424f46 greyish-blue
-                   "#d1b271",  ## #d1b271 lightbrownbeige
-                   "#FB4122",  ## #f14e28 orangered    	#F85639 lisa dress
-                   "#ABC67D",  ## #7bc242 applegreen     #ABC67D marge dress green
-                   "#0363C3", ## #007c7a teal    #0363C3 marge blue
-                   "#7A491E", ## #f7b686 beige    #7A491E
-                   "#000000",  ## #000000 black
-                   "#FC0209",   ## bart red
-                   "#46732EFF" ## Flanders green #46732EFF
+## dark mellem lys
+test_that("ucph palette dark outputs correct colours", {
+  expect_equal(ucph_pal("dark")(5),
+               c(      "#901a1E", # Rød
+                       "#122947", # Blå
+                       "#0a5963", # Petroleum
+                       "#39641c", # Grøn
+                       "#3d3d3d"  # Grå
                ))
 })
+
+test_that("ucph palette mellem outputs correct colours",{
+  expect_equal(ucph_pal("mellem")(6),
+               c(
+                 "#c73028", # Rød
+                 "#425570", # Blå
+                 "#197f8e", # Petroleum
+                 "#4b8325", # Grøn
+                 "#666666", # Grå
+                 "#fefaf2"  # Champagne
+               ))
+})
+
+test_that("ucph palette lys outputs correct colours",{
+  expect_equal(ucph_pal("lys")(6),
+               c(
+                 "#dB3B0A", # Rød
+                 "#bac7d9", # Blå
+                 "#b7d7de", # Petroleum
+                 "#becaa8", # Grøn
+                 "#e1dfdf", # Grå
+                 "#ffbd38"  # Gul
+               ))
+})
+
+test_that("ucph_pal warnings given if maximum values are supplied", {
+  expect_warning(ucph_pal("lys")(7))
+  expect_warning(ucph_pal("dark")(6))
+  expect_warning(ucph_pal("mellem")(7))
+})
+
